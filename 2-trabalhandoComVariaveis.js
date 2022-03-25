ecb0683305d892de448877b1bda7d95459e6a11b
Fabricio de Almeida Guerra de Melo <96056721+FabriicioMelo@users.noreply.github.com>
2022-03-25 17:05:18 -0300

    Adicionando 2-trabalhandoComVariaveis.js

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 16;
+const nome = "Fabrício";
+
+//O que não fazer
+mes = "Janeiro";
