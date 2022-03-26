9f47783043498a4744894e09d517067fa6ed78c9
Fabricio de Almeida Guerra de Melo <96056721+FabriicioMelo@users.noreply.github.com>
2022-03-26 01:56:54 -0300

    Adicionando 5-atribuicaoDeVariaveis.js

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const primeiroNome = "Fabrício";
+const ultimoSobrenome = "Melo";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, ultimoSobrenome);
+console.log(`Meu nome é ${primeiroNome} ${ultimoSobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + ultimoSobrenome;
+console.log(nomeCompleto);
+
+let idade;
+idade = 15;
+idade = idade + 1;
+console.log(`Eu tenho ${idade} anos`);
